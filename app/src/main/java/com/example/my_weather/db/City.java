7eb1cb6b5fb3a3e9c899998f1236bdb2e19b5af0
Gx927 <596086464@qq.com>
2020-11-27 14:13:16 +0800

    实现三级城市的遍历

---
+++ app/src/main/java/com/example/my_weather/db/City.java
@@ -0,0 +1,43 @@
+package com.example.my_weather.db;
+
+
+import org.litepal.crud.LitePalSupport;
+
+public class City extends LitePalSupport {
+    private int id;
+    private int provinceId;
+    private String cityName;
+    private int cityCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+}
